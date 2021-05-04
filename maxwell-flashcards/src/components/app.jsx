e766f00eb8ac396aae732f5db979fbddad243800
Sebastian Maxwell <maxwellsebas@gmail.com>
2021-05-03 22:40:03 -0400

    created index.js and app.jsx files. installed bootstrap

---
+++ maxwell-flashcards/src/components/app.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    state = { }
+    render() {
+        return (
+            <h1>Flashcards using a Component</h1>
+        );
+    }
+}
+
+export default App;
