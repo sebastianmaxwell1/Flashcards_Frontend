d35191b90620002624f8f73d6f2102534b1416fd
Sebastian Maxwell <maxwellsebas@gmail.com>
2021-05-05 16:54:01 -0400

    commented everthing out, restarting, put old code in temporary old file.

---
+++ maxwell-flashcards/src/attempt1/Card.js
@@ -0,0 +1,16 @@
+// import React, {useState} from 'react';
+
+// export default function Card({ frontSide, backSide }) {
+//     const [isFront, changeFace] = useState(true);;
+//     function handleClick() {
+//         changeFace(oldState => !oldState);
+//     }
+//     // const text = isFront ? frontSide : backSide;
+//     const sideClass = isFront ? "front" : "back";
+//     const classList = `flash-card ${sideClass}`;
+//     return (
+//         <div className={classList} onClick={handleClick}>
+//         {isFront ? frontSide : backSide}
+//         </div>
+//     );
+// }
---
+++ maxwell-flashcards/src/attempt1/app.jsx
@@ -0,0 +1,57 @@
+// import React, { Component } from 'react';
+// import './styles/styles.css'
+// import Card from './Card';
+// import axios from 'axios';
+
+
+// class App extends Component {
+//     constructor() {
+//         super()
+
+//         this.getAllFlashcards = this.getAllFlashcards.bind(this)
+//     }
+
+//     state = {
+//         flashcards: [],
+//         dataReady: false
+//     }
+
+//     componentDidMount(){
+//        this.getAllFlashcards();
+//     }
+
+//     async getAllFlashcards(){
+//         let response = await axios.get('http://127.0.0.1:8000/')
+//         this.setState({
+//             flashcards: response.data,
+//             dataReady: true
+//         });
+//         console.log(this.state.flashcards)
+//     }
+
+//     async addFlashcard(flashcardId){
+//         await axios.post('http://127.0.0.1:8000/flashcards/+/');
+//         this.getAllFlashcards();
+//     }
+    
+//     render() {
+//         if(this.state.dataReady){
+//             return(
+//                 <div className="App">
+//                 <h1>devCodeCampFlashcards</h1>
+//                 <h2>Click on a card for the answer!</h2>
+//                 <div>{this.state.flashcards.from()(function(question){
+//                         return  <Card frontSide={question.question} backSide={question.answer}/>
+//                     })}
+//                 </div>
+//            </div>
+//             )
+//         }
+//         else{
+//             return(<div></div>)
+//         }
+//     }
+// }
+
+// export default App;
+
